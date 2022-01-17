6066b7ca91c1379759875a52e2fea386078910d1
Kenix Whisperwind <kenixwhisperwind@gmail.com>
2022-01-17 16:59:58 -0500

    WindowShim now sets up the correct rendering and window API based on the target OS.

--- libultraship/WindowShim.c
+++ libultraship/WindowShim.c
@@ -1,7 +1,11 @@
 #include "PR/ultra64/gbi.h"
 #include "Lib/Fast3D/gfx_pc.h"
 #include "Lib/Fast3D/gfx_sdl.h"
+#include "Lib/Fast3D/gfx_dxgi.h"
+#include "Lib/Fast3D/gfx_glx.h"
 #include "Lib/Fast3D/gfx_opengl.h"
+#include "Lib/Fast3D/gfx_direct3d11.h"
+#include "Lib/Fast3D/gfx_direct3d12.h"
 #include "Lib/Fast3D/gfx_window_manager_api.h"
 
 /*
@@ -31,6 +35,18 @@ void ToggleConsole() {
  */
 
 void SetWindowManager(struct GfxWindowManagerAPI** WmApi, struct GfxRenderingAPI** RenderingApi) {
-	*WmApi = &gfx_sdl;
-	*RenderingApi = &gfx_opengl_api;
+#if defined(ENABLE_DX12)
+    *RenderingApi = &gfx_direct3d12_api;
+    *WmApi = &gfx_dxgi_api;
+#elif defined(ENABLE_DX11)
+    *RenderingApi = &gfx_direct3d11_api;
+    *WmApi = &gfx_dxgi_api;
+#elif defined(ENABLE_OPENGL)
+    *RenderingApi = &gfx_opengl_api;
+	#if defined(__linux__)
+	    *WmApi = &gfx_glx;
+	#else
+	    *WmApi = &gfx_sdl;
+	#endif
+#endif
 }
