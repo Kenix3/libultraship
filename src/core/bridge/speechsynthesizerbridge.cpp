38121693e4007300692b6a7417c220a362a2f4b7
David Chavez <david@dcvz.io>
2022-11-23 20:03:11 +0100

    [Feature] Add API for speech synthesis (#44)
    * Add API for speech synthesis
    
    * Fix CI builds
    
    * Tidy fixes
    
    * Fix lint issue
    
    * Use h instead of hpp
    
    * Fix CI issues
    
    * Add missing bridge method
    
    * Small style change

---
+++ src/core/bridge/speechsynthesizerbridge.cpp
@@ -0,0 +1,34 @@
+//
+//  speechsynthesizerbridge.cpp
+//  libultraship
+//
+//  Created by David Chavez on 22.11.22.
+//
+
+#include "speechsynthesizerbridge.h"
+#include "core/Window.h"
+
+extern "C" {
+
+bool SpeechSynthesizerInit(void) {
+    auto speechSynthesizer = Ship::Window::GetInstance()->GetSpeechSynthesizer();
+    if (speechSynthesizer == nullptr) {
+        return false;
+    }
+
+    if (speechSynthesizer->Init()) {
+        return true;
+    }
+
+    return false;
+}
+
+void SpeechSynthesizerSpeak(const char* text) {
+    auto speechSynthesizer = Ship::Window::GetInstance()->GetSpeechSynthesizer();
+    if (speechSynthesizer == nullptr) {
+        return;
+    }
+
+    speechSynthesizer->Speak(text);
+}
+}
---
+++ src/speechsynthesizer/SAPISpeechSynthesizer.cpp
@@ -0,0 +1,40 @@
+//
+//  SAPISpeechSynthesizer.cpp
+//  libultraship
+//
+//  Created by David Chavez on 22.11.22.
+//
+
+#include "SAPISpeechSynthesizer.h"
+#include <sapi.h>
+#include <thread>
+
+ISpVoice* mVoice = NULL;
+
+namespace Ship {
+SAPISpeechSynthesizer::SAPISpeechSynthesizer() {
+}
+
+bool SAPISpeechSynthesizer::DoInit() {
+    CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    HRESULT CoInitializeEx(LPVOID pvReserved, DWORD dwCoInit);
+    CoCreateInstance(CLSID_SpVoice, NULL, CLSCTX_ALL, IID_ISpVoice, (void**)&mVoice);
+    return true;
+}
+
+void SpeakThreadTask(const char* text) {
+    const int w = 512;
+    int* wp = const_cast<int*>(&w);
+    *wp = strlen(text);
+
+    wchar_t wtext[w];
+    mbstowcs(wtext, text, strlen(text) + 1);
+
+    mVoice->Speak(wtext, SPF_IS_XML | SPF_ASYNC | SPF_PURGEBEFORESPEAK, NULL);
+}
+
+void SAPISpeechSynthesizer::Speak(const char* text) {
+    std::thread t1(SpeakThreadTask, text);
+    t1.detach();
+}
+} // namespace Ship
