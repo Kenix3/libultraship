41235bbab98778d1b06c76d68f2a34b626e58a29
David Chavez <david@dcvz.io>
2023-01-11 17:39:31 +0100

    Use Metal single header

--- src/graphic/Fast3D/gfx_metal.cpp
+++ src/graphic/Fast3D/gfx_metal.cpp
@@ -26,9 +26,7 @@
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
-#include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
-#include <QuartzCore/QuartzCore.hpp>
 #include <SDL_render.h>
 #include <ImGui/backends/imgui_impl_metal.h>
 #include <spdlog/spdlog.h>
