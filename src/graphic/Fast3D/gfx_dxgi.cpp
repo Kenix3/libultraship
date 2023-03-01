6ef4bc3590773e3bd5186ac49633e519c0446868
aMannus <mannusmenting@gmail.com>
2023-03-01 23:45:03 +0100

    Force vsync interval to 1

--- src/graphic/Fast3D/gfx_dxgi.cpp
+++ src/graphic/Fast3D/gfx_dxgi.cpp
@@ -563,7 +563,7 @@ static bool gfx_dxgi_start_frame(void) {
 }
 
 static void gfx_dxgi_swap_buffers_begin(void) {
-    // dxgi.length_in_vsync_frames = 1;
+    dxgi.length_in_vsync_frames = 1;
     LARGE_INTEGER t;
     if (dxgi.use_timer) {
         QueryPerformanceCounter(&t);
