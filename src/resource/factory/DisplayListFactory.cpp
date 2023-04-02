9ebed2afa8125c31264d510e4ce5cc35beaca8ef
Nicholas Estelami <NEstelami@users.noreply.github.com>
2023-04-02 10:38:49 -0400

    Custom model support fixes (#153)
    * GBI Macro fixes
    
    * Clang-format run
    
    * make format happy
    
    * make tidy happy
    
    * make format happy
    
    ---------
    
    Co-authored-by: briaguya <briaguya>

--- src/resource/factory/DisplayListFactory.cpp
+++ src/resource/factory/DisplayListFactory.cpp
@@ -115,6 +115,22 @@ std::unordered_map<std::string, uint32_t> renderModes = { { "G_RM_ZB_OPA_SURF", 
                                                           { "G_RM_ZB_XLU_DECAL2", G_RM_ZB_XLU_DECAL2 },
                                                           { "G_RM_ZB_CLD_SURF2", G_RM_ZB_CLD_SURF2 } };
 
+static Gfx GsSpVertexOtR2P1(char* filePathPtr) {
+    Gfx g;
+    g.words.w0 = G_VTX_OTR_FILEPATH << 24;
+    g.words.w1 = (uintptr_t)filePathPtr;
+
+    return g;
+}
+
+static Gfx GsSpVertexOtR2P2(int vtxCnt, int vtxBufOffset, int vtxDataOffset) {
+    Gfx g;
+    g.words.w0 = (uintptr_t)vtxCnt;
+    g.words.w1 = (uintptr_t)((vtxBufOffset << 16) | vtxDataOffset);
+
+    return g;
+}
+
 void DisplayListFactoryV0::ParseFileXML(tinyxml2::XMLElement* reader, std::shared_ptr<Resource> resource) {
     std::shared_ptr<DisplayList> dl = std::static_pointer_cast<DisplayList>(resource);
 
@@ -317,8 +333,7 @@ void DisplayListFactoryV0::ParseFileXML(tinyxml2::XMLElement* reader, std::share
             int v01 = child->IntAttribute("V01");
             int v02 = child->IntAttribute("V02");
 
-            g = gsSP1TriangleOTR(child->IntAttribute("V00"), child->IntAttribute("V01"), child->IntAttribute("V02"),
-                                 child->IntAttribute("Flag0"));
+            g = gsSP1TriangleOTR(v00, v01, v02, child->IntAttribute("Flag0"));
             g.words.w0 &= 0xFF000000;
             g.words.w0 |= v00;
             g.words.w1 |= v01 << 16;
@@ -334,12 +349,12 @@ void DisplayListFactoryV0::ParseFileXML(tinyxml2::XMLElement* reader, std::share
             char* filePath = (char*)malloc(fName.size() + 1);
             strcpy(filePath, fName.data());
 
-            g = gsSPVertexOTR2_P1(filePath);
+            g = GsSpVertexOtR2P1(filePath);
 
             dl->Instructions.push_back(g);
 
-            g = gsSPVertexOTR2_P2(child->IntAttribute("Count"), child->IntAttribute("VertexBufferIndex"),
-                                  child->IntAttribute("VertexOffset"));
+            g = GsSpVertexOtR2P2(child->IntAttribute("Count"), child->IntAttribute("VertexBufferIndex"),
+                                 child->IntAttribute("VertexOffset"));
         } else if (childName == "SetTextureImage") {
             std::string fName = child->Attribute("Path");
             // fName = ">" + fName;
