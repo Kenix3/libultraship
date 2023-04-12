2ab5968e3d8449378aa11e96539f3776104fe124
Adam Bird <archez39@me.com>
2023-04-12 01:07:01 -0400

    add methods to get macos app bundle path

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -145,6 +145,21 @@ void Window::Close() {
     mWindowManagerApi->close();
 }
 
+std::string Window::GetAppBundlePath() {
+#ifdef __APPLE__
+    FolderManager folderManager;
+    return folderManager.getMainBundlePath();
+#endif
+
+#ifdef __linux__
+    char* fpath = std::getenv("SHIP_HOME");
+    if (fpath != NULL)
+        return std::string(fpath);
+#endif
+
+    return ".";
+}
+
 std::string Window::GetAppDirectoryPath() {
 #ifdef __APPLE__
     FolderManager folderManager;
@@ -160,6 +175,10 @@ std::string Window::GetAppDirectoryPath() {
 #endif
 
     return ".";
+}
+
+std::string Window::GetPathRelativeToAppBundle(const char* path) {
+    return GetAppBundlePath() + "/" + path;
 }
 
 std::string Window::GetPathRelativeToAppDirectory(const char* path) {
