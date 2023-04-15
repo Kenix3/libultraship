6f3d8711bc72156a3b604fdb835788be34f51853
Ralphie Morell <stratomaster64@gmail.com>
2023-04-15 01:38:51 -0400

    LUS Scancodes (#165)
    * Add LUS scancodes; renamed lookup tables
    
    * Add KB scancodes to `classes` header
    
    * Revert !163 in favor of LUS scancodes
    
    * linting
    
    * Updated KbScancode to use Windows scancodes instead of keycodes
    
    * Cleanup

--- src/graphic/Fast3D/gfx_dxgi.cpp
+++ src/graphic/Fast3D/gfx_dxgi.cpp
@@ -216,15 +216,13 @@ static void toggle_borderless_window_full_screen(bool enable, bool call_callback
 }
 
 static void onkeydown(WPARAM w_param, LPARAM l_param) {
-    int scancode = ((l_param >> 16) & 0x1ff);
-    int key = scancode < 512 ? windows_scancode_table[scancode] + 1 : 0;
+    int key = ((l_param >> 16) & 0x1ff);
     if (dxgi.on_key_down != nullptr) {
         dxgi.on_key_down(key);
     }
 }
 static void onkeyup(WPARAM w_param, LPARAM l_param) {
-    int scancode = ((l_param >> 16) & 0x1ff);
-    int key = scancode < 512 ? windows_scancode_table[scancode] + 1 : 0;
+    int key = ((l_param >> 16) & 0x1ff);
     if (dxgi.on_key_up != nullptr) {
         dxgi.on_key_up(key);
     }
