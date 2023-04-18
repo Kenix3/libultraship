bc70adc134d05ae67b35a6dccceab36a640eca90
Kenix <kenixwhisperwind@gmail.com>
2023-04-18 01:17:45 -0400

    SwappableAssetPrefix -> gHdAssetPrefix

--- src/resource/ResourceMgr.cpp
+++ src/resource/ResourceMgr.cpp
@@ -76,8 +76,8 @@ std::shared_ptr<Resource> ResourceMgr::LoadResourceProcess(const std::string& fi
     }
 
     // Attempt to load the HD version of the asset, if we fail then we continue trying to load the standard asset.
-    if (!loadExact && CVarGetInteger("gHdAssets", 0) && !filePath.starts_with(Resource::SwappableAssetPrefix)) {
-        const auto hdPath = Resource::SwappableAssetPrefix + filePath;
+    if (!loadExact && CVarGetInteger("gHdAssets", 0) && !filePath.starts_with(Resource::gHdAssetPrefix)) {
+        const auto hdPath = Resource::gHdAssetPrefix + filePath;
         auto hdResource = LoadResourceProcess(hdPath, loadExact);
 
         if (hdResource != nullptr) {
@@ -94,7 +94,7 @@ std::shared_ptr<Resource> ResourceMgr::LoadResourceProcess(const std::string& fi
     }
     // Check for resource load errors which can indicate an HD asset.
     // If we are attempting to load an HD asset, we can return null
-    if (!loadExact && CVarGetInteger("gHdAssets", 0) && filePath.starts_with(Resource::SwappableAssetPrefix)) {
+    if (!loadExact && CVarGetInteger("gHdAssets", 0) && filePath.starts_with(Resource::gHdAssetPrefix)) {
         if (std::holds_alternative<ResourceLoadError>(cacheLine)) {
             try {
                 // If we have attempted to cache an HD asset, but failed, we return nullptr and rely on the calling
@@ -189,8 +189,8 @@ std::shared_ptr<Resource> ResourceMgr::LoadResource(const std::string& filePath)
 
 std::variant<ResourceMgr::ResourceLoadError, std::shared_ptr<Resource>>
 ResourceMgr::CheckCache(const std::string& filePath, bool loadExact) {
-    if (!loadExact && CVarGetInteger("gHdAssets", 0) && !filePath.starts_with(Resource::SwappableAssetPrefix)) {
-        const auto hdPath = Resource::SwappableAssetPrefix + filePath;
+    if (!loadExact && CVarGetInteger("gHdAssets", 0) && !filePath.starts_with(Resource::gHdAssetPrefix)) {
+        const auto hdPath = Resource::gHdAssetPrefix + filePath;
         auto hdCacheResult = CheckCache(hdPath, loadExact);
 
         // If the type held at this cache index is a resource, then we return it.
