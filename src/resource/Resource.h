e1a567401428ab14019473e0bb89c932b8da6862
Kenix <kenixwhisperwind@gmail.com>
2023-04-18 01:10:48 -0400

    SwappableAssetPrefix -> gHdAssetPrefix

--- src/resource/Resource.h
+++ src/resource/Resource.h
@@ -19,7 +19,7 @@ struct ResourceInitData {
 
 class Resource {
   public:
-    inline static const std::string SwappableAssetPrefix = "hd/";
+    inline static const std::string gHdAssetPrefix = "hd/";
 
     std::shared_ptr<ResourceMgr> ResourceManager;
     std::shared_ptr<ResourceInitData> InitData;
