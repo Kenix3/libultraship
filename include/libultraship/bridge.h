04e8dfab2faba7b1b7942e9ab015a5aecdec5c42
Kenix <kenixwhisperwind@gmail.com>
2023-05-17 00:12:21 -0400

    audioplayerbridge -> audioplayer

--- include/libultraship/bridge.h
+++ include/libultraship/bridge.h
@@ -2,7 +2,7 @@
 #define _BRIDGE_H_
 
 #include "core/bridge/resourcebridge.h"
-#include "core/bridge/audioplayerbridge.h"
+#include "core/bridge/audiobridge.h"
 #include "core/bridge/controldeckbridge.h"
 #include "core/bridge/windowbridge.h"
 #include "core/bridge/consolevariablebridge.h"
