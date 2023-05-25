2e67eac40452bc095a30e81267630cd5c9f682a9
Kenix <kenixwhisperwind@gmail.com>
2023-05-24 20:58:50 -0400

    Runs clang-format

--- src/window/gui/InputEditorWindow.cpp
+++ src/window/gui/InputEditorWindow.cpp
@@ -50,8 +50,10 @@ void InputEditorWindow::DrawButton(const char* label, int32_t n64Btn, int32_t cu
 
         if (btn != -1) {
             auto profile = backend->GetProfile(currentPort);
-            // Remove other mappings that include the n64 button. Note that the n64 button is really a mask and is not unique, but the UI as-is needs a way to unset the old n64 button.
-            std::erase_if(profile->Mappings, [n64Btn](const std::pair<int32_t, int32_t>& bin) { return bin.second == n64Btn; });
+            // Remove other mappings that include the n64 button. Note that the n64 button is really a mask and is not
+            // unique, but the UI as-is needs a way to unset the old n64 button.
+            std::erase_if(profile->Mappings,
+                          [n64Btn](const std::pair<int32_t, int32_t>& bin) { return bin.second == n64Btn; });
             backend->SetButtonMapping(currentPort, n64Btn, btn);
             auto keyboardBackend = dynamic_pointer_cast<KeyboardController>(backend);
             if (keyboardBackend != nullptr) {
