e2c6b7c20fd72dea325892ca7da5a94ff95a4857
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-09-24 09:14:03 -0400

    axis button names

--- src/controller/sdl/SDLAxisDirectionToButtonMapping.cpp
+++ src/controller/sdl/SDLAxisDirectionToButtonMapping.cpp
@@ -1,5 +1,7 @@
 #include "SDLAxisDirectionToButtonMapping.h"
 #include <spdlog/spdlog.h>
+#include <Utils/StringHelper.h>
+#include "window/gui/IconsFontAwesome4.h"
 
 namespace LUS {
 SDLAxisDirectionToButtonMapping::SDLAxisDirectionToButtonMapping(uint16_t bitmask, int32_t sdlControllerIndex,
@@ -28,7 +30,39 @@ uint8_t SDLAxisDirectionToButtonMapping::GetMappingType() {
 }
 
 std::string SDLAxisDirectionToButtonMapping::GetButtonName() {
-    auto blarg = mController;
-    return "blarg";
+    switch (mControllerAxis) {
+        case SDL_CONTROLLER_AXIS_LEFTX:
+            return StringHelper::Sprintf("Left Stick %s", mAxisDirection == NEGATIVE ? ICON_FA_ARROW_LEFT : ICON_FA_ARROW_RIGHT);
+        case SDL_CONTROLLER_AXIS_LEFTY:
+            return StringHelper::Sprintf("Left Stick %s", mAxisDirection == NEGATIVE ? ICON_FA_ARROW_UP : ICON_FA_ARROW_DOWN);
+        case SDL_CONTROLLER_AXIS_RIGHTX:
+            return StringHelper::Sprintf("Right Stick %s", mAxisDirection == NEGATIVE ? ICON_FA_ARROW_LEFT : ICON_FA_ARROW_RIGHT);
+        case SDL_CONTROLLER_AXIS_RIGHTY:
+            return StringHelper::Sprintf("Right Stick %s", mAxisDirection == NEGATIVE ? ICON_FA_ARROW_UP : ICON_FA_ARROW_DOWN);
+        case SDL_CONTROLLER_AXIS_TRIGGERLEFT:
+            if (UsesPlaystationLayout()) {
+                return "L2";
+            }
+            if (UsesSwitchLayout()) {
+                return "LR";
+            }
+            if (UsesXboxLayout()) {
+                return "LT";
+            }
+            break;
+        case SDL_CONTROLLER_AXIS_TRIGGERRIGHT:
+            if (UsesPlaystationLayout()) {
+                return "R2";
+            }
+            if (UsesSwitchLayout()) {
+                return "ZR";
+            }
+            if (UsesXboxLayout()) {
+                return "RT";
+            }
+            break;
+    }
+
+    return StringHelper::Sprintf("Axis %d %s", mControllerAxis, mAxisDirection == NEGATIVE ? ICON_FA_MINUS : ICON_FA_PLUS);
 }
 } // namespace LUS
