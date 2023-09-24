8b2bd94052f4da6bc61ff3967d2b76970b9c5fb2
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-09-24 05:00:18 -0400

    builds with uuid hackaround

--- src/controller/ButtonMapping.cpp
+++ src/controller/ButtonMapping.cpp
@@ -1,7 +1,15 @@
 #include "ButtonMapping.h"
 
+#include <random>
+#include <sstream>
+
 namespace LUS {
 ButtonMapping::ButtonMapping(uint16_t bitmask) : mBitmask(bitmask) {
+    GenerateUuid();
+}
+
+ButtonMapping::ButtonMapping(uint16_t bitmask, std::string uuid) : mBitmask(bitmask) {
+    mUuid = uuid;
 }
 
 ButtonMapping::~ButtonMapping() {
@@ -9,5 +17,45 @@ ButtonMapping::~ButtonMapping() {
 
 uint16_t ButtonMapping::GetBitmask() {
     return mBitmask;
+}
+
+std::string ButtonMapping::GetUuid() {
+    return mUuid;
+}
+
+void ButtonMapping::GenerateUuid() {
+    // todo: this a better way
+    // i tried some cross-platform uuid lib stuff and ended up fighting cmake
+    // so i figured i'd just this for now despite the fact that has been advised
+    // against on stackoverflow: https://stackoverflow.com/a/60198074
+    static std::random_device              rd;
+    static std::mt19937                    gen(rd());
+    static std::uniform_int_distribution<> dis(0, 15);
+    static std::uniform_int_distribution<> dis2(8, 11);
+    
+    std::stringstream ss;
+    int i;
+    ss << std::hex;
+    for (i = 0; i < 8; i++) {
+        ss << dis(gen);
+    }
+    ss << "-";
+    for (i = 0; i < 4; i++) {
+        ss << dis(gen);
+    }
+    ss << "-4";
+    for (i = 0; i < 3; i++) {
+        ss << dis(gen);
+    }
+    ss << "-";
+    ss << dis2(gen);
+    for (i = 0; i < 3; i++) {
+        ss << dis(gen);
+    }
+    ss << "-";
+    for (i = 0; i < 12; i++) {
+        ss << dis(gen);
+    };
+    mUuid = ss.str();
 }
 } // namespace LUS
--- src/controller/ButtonMapping.h
+++ src/controller/ButtonMapping.h
@@ -1,17 +1,25 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 namespace LUS {
 class ButtonMapping {
   public:
     ButtonMapping(uint16_t bitmask);
+    ButtonMapping(uint16_t bitmask, std::string uuid);
     ~ButtonMapping();
+
+    std::string GetUuid();
 
     uint16_t GetBitmask();
     virtual void UpdatePad(uint16_t& padButtons) = 0;
 
   protected:
     uint16_t mBitmask;
+    std::string mUuid;
+  
+  private:
+    void GenerateUuid();
 };
 } // namespace LUS
