6d780a5f31390d7f6c13e9462f664258d9f5aeb8
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-09-29 09:37:37 -0400

    uncomment

--- src/public/bridge/controllerbridge.cpp
+++ src/public/bridge/controllerbridge.cpp
@@ -3,11 +3,11 @@
 
 extern "C" {
 
-// void ControllerBlockGameInput(uint16_t inputBlockId) {
-//     LUS::Context::GetInstance()->GetControlDeck()->BlockGameInput(static_cast<int32_t>(inputBlockId));
-// }
+void ControllerBlockGameInput(uint16_t inputBlockId) {
+    LUS::Context::GetInstance()->GetControlDeck()->BlockGameInput(static_cast<int32_t>(inputBlockId));
+}
 
-// void ControllerUnblockGameInput(uint16_t inputBlockId) {
-//     LUS::Context::GetInstance()->GetControlDeck()->UnblockGameInput(static_cast<int32_t>(inputBlockId));
-// }
+void ControllerUnblockGameInput(uint16_t inputBlockId) {
+    LUS::Context::GetInstance()->GetControlDeck()->UnblockGameInput(static_cast<int32_t>(inputBlockId));
+}
 }
