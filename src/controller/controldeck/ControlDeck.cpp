a5bbcf8647056c8249f4765dc0a6a8d8b62dd1cf
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-10-02 04:32:49 -0400

    cleanup commented stuff

--- src/controller/controldeck/ControlDeck.cpp
+++ src/controller/controldeck/ControlDeck.cpp
@@ -51,54 +51,6 @@ bool ControlDeck::ProcessKeyboardEvent(KbEventType eventType, KbScancode scancod
     return result;
 }
 
-// void ControlDeck::ScanDevices() {
-//     mPortList.clear();
-//     mDevices.clear();
-
-//     // Always load controllers that need their device indices zero based first because we add some other devices
-//     // afterward.
-//     int32_t i;
-
-// #ifndef __WIIU__
-//     for (i = 0; i < SDL_NumJoysticks(); i++) {
-//         if (SDL_IsGameController(i)) {
-//             auto sdl = std::make_shared<SDLController>(i);
-//             sdl->Open();
-//             mDevices.push_back(sdl);
-//         }
-//     }
-
-//     mDevices.push_back(std::make_shared<DummyController>(i++, "Auto", "Auto", true));
-//     mDevices.push_back(std::make_shared<KeyboardController>(i++));
-// #else
-//     for (i = 0; i < 4; i++) {
-//         auto controller = std::make_shared<LUS::WiiUController>(i, (WPADChan)i);
-//         controller->Open();
-//         mDevices.push_back(controller);
-//     }
-
-//     auto gamepad = std::make_shared<LUS::WiiUGamepad>(i++);
-//     gamepad->Open();
-//     mDevices.push_back(gamepad);
-
-//     mDevices.push_back(std::make_shared<DummyController>(i++, "Auto", "Auto", true));
-// #endif
-
-//     mDevices.push_back(std::make_shared<DummyController>(i++, "Disconnected", "None", false));
-
-//     for (const auto& device : mDevices) {
-//         for (int32_t i = 0; i < MAXCONTROLLERS; i++) {
-//             device->CreateDefaultBinding(i);
-//         }
-//     }
-
-//     for (int32_t i = 0; i < MAXCONTROLLERS; i++) {
-//         mPortList.push_back(i == 0 ? 0 : static_cast<int>(mDevices.size()) - 1);
-//     }
-
-//     LoadSettings();
-// }
-
 bool ControlDeck::AllGameInputBlocked() {
     return !mGameInputBlockers.empty();
 }
@@ -137,26 +89,6 @@ std::shared_ptr<Controller> ControlDeck::GetControllerByPort(uint8_t port) {
     return mPorts[port]->GetConnectedController();
 }
 
-// size_t ControlDeck::GetNumDevices() {
-//     return mDevices.size();
-// }
-
-// int32_t ControlDeck::GetDeviceIndexFromPortIndex(int32_t portIndex) {
-//     return mPortList[portIndex];
-// }
-
-// size_t ControlDeck::GetNumConnectedPorts() {
-//     return mPortList.size();
-// }
-
-// std::shared_ptr<Controller> ControlDeck::GetDeviceFromPortIndex(int32_t portIndex) {
-//     return GetDeviceFromDeviceIndex(GetDeviceIndexFromPortIndex(portIndex));
-// }
-
-// uint8_t* ControlDeck::GetControllerBits() {
-//     return mControllerBits;
-// }
-
 void ControlDeck::BlockGameInput(int32_t blockId) {
     mGameInputBlockers[blockId] = true;
 }
@@ -164,19 +96,4 @@ void ControlDeck::BlockGameInput(int32_t blockId) {
 void ControlDeck::UnblockGameInput(int32_t blockId) {
     mGameInputBlockers.erase(blockId);
 }
-
-// bool ControlDeck::IsBlockingGameInput(const std::string& inputDeviceGuid) const {
-//     // We block controller input if F1 menu is open and control navigation is on.
-//     // This is because we don't want controller inputs to affect the game
-//     bool shouldBlockControllerInput = CVarGetInteger("gOpenMenuBar", 0) && CVarGetInteger("gControlNav", 0);
-
-//     // We block keyboard input if you're currently typing into a textfield.
-//     // This is because we don't want your keyboard typing to affect the game.
-//     ImGuiIO io = ImGui::GetIO();
-//     bool shouldBlockKeyboardInput = io.WantCaptureKeyboard;
-
-//     bool inputDeviceIsKeyboard = inputDeviceGuid == "Keyboard";
-//     return (!mGameInputBlockers.empty()) ||
-//            (inputDeviceIsKeyboard ? shouldBlockKeyboardInput : shouldBlockControllerInput);
-// }
 } // namespace LUS
