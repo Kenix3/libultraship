11d134862d0c6f82cc7c5394f37e086ba92bf07f
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-11-04 08:45:38 -0400

    fix wii u build

--- src/window/gui/InputEditorWindow.cpp
+++ src/window/gui/InputEditorWindow.cpp
@@ -1110,71 +1110,6 @@ void InputEditorWindow::DrawLEDDeviceIcons(uint8_t portIndex) {
     }
 }
 
-void InputEditorWindow::DrawSetDefaultsButton(uint8_t portIndex) {
-    ImGui::SameLine();
-    ImGui::PushStyleVar(ImGuiStyleVar_ButtonTextAlign, ImVec2(1.0f, 0.5f));
-    auto popupId = StringHelper::Sprintf("setDefaultsPopup##%d", portIndex);
-    if (ImGui::Button(StringHelper::Sprintf("Set defaults...##%d", portIndex).c_str())) {
-        ImGui::OpenPopup(popupId.c_str());
-    }
-    ImGui::PopStyleVar();
-
-    if (ImGui::BeginPopup(popupId.c_str())) {
-        std::map<LUSDeviceIndex, std::pair<std::string, int32_t>> indexMappings;
-        for (auto [lusIndex, mapping] :
-             Context::GetInstance()->GetControlDeck()->GetDeviceIndexMappingManager()->GetAllDeviceIndexMappings()) {
-            auto sdlIndexMapping = std::static_pointer_cast<LUSDeviceIndexToSDLDeviceIndexMapping>(mapping);
-            if (sdlIndexMapping == nullptr) {
-                continue;
-            }
-
-            indexMappings[lusIndex] = { sdlIndexMapping->GetSDLControllerName(), sdlIndexMapping->GetSDLDeviceIndex() };
-        }
-
-        bool shouldClose = false;
-        for (auto [lusIndex, info] : indexMappings) {
-            auto [name, sdlIndex] = info;
-
-            auto buttonColor = ImGui::GetStyleColorVec4(ImGuiCol_Button);
-            auto buttonHoveredColor = ImGui::GetStyleColorVec4(ImGuiCol_ButtonHovered);
-            GetButtonColorsForLUSDeviceIndex(lusIndex, buttonColor, buttonHoveredColor);
-            ImGui::PushStyleColor(ImGuiCol_Button, buttonColor);
-            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, buttonHoveredColor);
-            if (ImGui::Button(StringHelper::Sprintf("%s %s (%s)", ICON_FA_GAMEPAD, name.c_str(),
-                                                    StringHelper::Sprintf("SDL %d", sdlIndex).c_str())
-                                  .c_str())) {
-                ImGui::OpenPopup(StringHelper::Sprintf("Set Defaults for %s", name.c_str()).c_str());
-            }
-            ImGui::PopStyleColor();
-            ImGui::PopStyleColor();
-            if (ImGui::BeginPopupModal(StringHelper::Sprintf("Set Defaults for %s", name.c_str()).c_str(), NULL,
-                                       ImGuiWindowFlags_AlwaysAutoResize)) {
-                ImGui::Text("This will clear all existing mappings for\n%s (SDL %d) on port %d.\n\nContinue?",
-                            name.c_str(), sdlIndex, portIndex + 1);
-                if (ImGui::Button("Cancel")) {
-                    shouldClose = true;
-                    ImGui::CloseCurrentPopup();
-                }
-                if (ImGui::Button("Set defaults")) {
-                    Context::GetInstance()->GetControlDeck()->GetControllerByPort(portIndex)->ClearAllMappingsForDevice(
-                        lusIndex);
-                    Context::GetInstance()->GetControlDeck()->GetControllerByPort(portIndex)->AddDefaultMappings(
-                        lusIndex);
-                    shouldClose = true;
-                    ImGui::CloseCurrentPopup();
-                }
-                ImGui::EndPopup();
-            }
-        }
-
-        if (ImGui::Button("Cancel") || shouldClose) {
-            ImGui::CloseCurrentPopup();
-        }
-
-        ImGui::EndPopup();
-    }
-}
-
 void InputEditorWindow::DrawPortTab(uint8_t portIndex) {
     if (ImGui::BeginTabItem(StringHelper::Sprintf("Port %d###port%d", portIndex + 1, portIndex).c_str())) {
         if (ImGui::Button("Clear All")) {
@@ -1279,6 +1214,76 @@ void InputEditorWindow::DrawPortTab(uint8_t portIndex) {
 }
 
 #ifdef __WIIU__
+void InputEditorWindow::DrawSetDefaultsButton(uint8_t portIndex) {
+    ImGui::SameLine();
+    ImGui::PushStyleVar(ImGuiStyleVar_ButtonTextAlign, ImVec2(1.0f, 0.5f));
+    auto popupId = StringHelper::Sprintf("setDefaultsPopup##%d", portIndex);
+    if (ImGui::Button(StringHelper::Sprintf("Set defaults...##%d", portIndex).c_str())) {
+        ImGui::OpenPopup(popupId.c_str());
+    }
+    ImGui::PopStyleVar();
+
+    if (ImGui::BeginPopup(popupId.c_str())) {
+        std::map<LUSDeviceIndex, std::pair<std::string, int32_t>> indexMappings;
+        for (auto [lusIndex, mapping] :
+             Context::GetInstance()->GetControlDeck()->GetDeviceIndexMappingManager()->GetAllDeviceIndexMappings()) {
+            auto wiiuIndexMapping = std::static_pointer_cast<LUSDeviceIndexToWiiUDeviceIndexMapping>(mapping);
+            if (wiiuIndexMapping == nullptr) {
+                continue;
+            }
+
+            indexMappings[lusIndex] = { wiiuIndexMapping->GetWiiUControllerName(), wiiuIndexMapping->IsWiiUGamepad() ? INT32_MAX
+                                                                          : wiiuIndexMapping->GetDeviceChannel() };
+        }
+
+        bool shouldClose = false;
+        for (auto [lusIndex, info] : indexMappings) {
+            auto [name, wiiuChannel] = info;
+            bool isGamepad = wiiuChannel == INT32_MAX;
+
+            auto buttonColor = ImGui::GetStyleColorVec4(ImGuiCol_Button);
+            auto buttonHoveredColor = ImGui::GetStyleColorVec4(ImGuiCol_ButtonHovered);
+            GetButtonColorsForLUSDeviceIndex(lusIndex, buttonColor, buttonHoveredColor);
+            ImGui::PushStyleColor(ImGuiCol_Button, buttonColor);
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, buttonHoveredColor);
+
+            auto fancyName = StringHelper::Sprintf("%s%s",
+                                                name.c_str(),
+                                                isGamepad ? ""
+                                                            : StringHelper::Sprintf(" (%d)", wiiuChannel).c_str());
+            if(ImGui::Button(StringHelper::Sprintf("%s %s", ICON_FA_GAMEPAD, fancyName.c_str()).c_str())) {
+                ImGui::OpenPopup(StringHelper::Sprintf("Set Defaults for %s", name.c_str()).c_str());
+            }
+            ImGui::PopStyleColor();
+            ImGui::PopStyleColor();
+            if (ImGui::BeginPopupModal(StringHelper::Sprintf("Set Defaults for %s", name.c_str()).c_str(), NULL,
+                                       ImGuiWindowFlags_AlwaysAutoResize)) {
+                ImGui::Text("This will clear all existing mappings for\n%s on port %d.\n\nContinue?",
+                            fancyName.c_str(), portIndex + 1);
+                if (ImGui::Button("Cancel")) {
+                    shouldClose = true;
+                    ImGui::CloseCurrentPopup();
+                }
+                if (ImGui::Button("Set defaults")) {
+                    Context::GetInstance()->GetControlDeck()->GetControllerByPort(portIndex)->ClearAllMappingsForDevice(
+                        lusIndex);
+                    Context::GetInstance()->GetControlDeck()->GetControllerByPort(portIndex)->AddDefaultMappings(
+                        lusIndex);
+                    shouldClose = true;
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+        }
+
+        if (ImGui::Button("Cancel") || shouldClose) {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
 void InputEditorWindow::DrawDevicesTab() {
     if (ImGui::BeginTabItem("Devices")) {
         std::map<LUSDeviceIndex, std::pair<std::string, int32_t>> indexMappings;
@@ -1330,6 +1335,71 @@ void InputEditorWindow::DrawDevicesTab() {
     }
 }
 #else
+void InputEditorWindow::DrawSetDefaultsButton(uint8_t portIndex) {
+    ImGui::SameLine();
+    ImGui::PushStyleVar(ImGuiStyleVar_ButtonTextAlign, ImVec2(1.0f, 0.5f));
+    auto popupId = StringHelper::Sprintf("setDefaultsPopup##%d", portIndex);
+    if (ImGui::Button(StringHelper::Sprintf("Set defaults...##%d", portIndex).c_str())) {
+        ImGui::OpenPopup(popupId.c_str());
+    }
+    ImGui::PopStyleVar();
+
+    if (ImGui::BeginPopup(popupId.c_str())) {
+        std::map<LUSDeviceIndex, std::pair<std::string, int32_t>> indexMappings;
+        for (auto [lusIndex, mapping] :
+             Context::GetInstance()->GetControlDeck()->GetDeviceIndexMappingManager()->GetAllDeviceIndexMappings()) {
+            auto sdlIndexMapping = std::static_pointer_cast<LUSDeviceIndexToSDLDeviceIndexMapping>(mapping);
+            if (sdlIndexMapping == nullptr) {
+                continue;
+            }
+
+            indexMappings[lusIndex] = { sdlIndexMapping->GetSDLControllerName(), sdlIndexMapping->GetSDLDeviceIndex() };
+        }
+
+        bool shouldClose = false;
+        for (auto [lusIndex, info] : indexMappings) {
+            auto [name, sdlIndex] = info;
+
+            auto buttonColor = ImGui::GetStyleColorVec4(ImGuiCol_Button);
+            auto buttonHoveredColor = ImGui::GetStyleColorVec4(ImGuiCol_ButtonHovered);
+            GetButtonColorsForLUSDeviceIndex(lusIndex, buttonColor, buttonHoveredColor);
+            ImGui::PushStyleColor(ImGuiCol_Button, buttonColor);
+            ImGui::PushStyleColor(ImGuiCol_ButtonHovered, buttonHoveredColor);
+            if (ImGui::Button(StringHelper::Sprintf("%s %s (%s)", ICON_FA_GAMEPAD, name.c_str(),
+                                                    StringHelper::Sprintf("SDL %d", sdlIndex).c_str())
+                                  .c_str())) {
+                ImGui::OpenPopup(StringHelper::Sprintf("Set Defaults for %s", name.c_str()).c_str());
+            }
+            ImGui::PopStyleColor();
+            ImGui::PopStyleColor();
+            if (ImGui::BeginPopupModal(StringHelper::Sprintf("Set Defaults for %s", name.c_str()).c_str(), NULL,
+                                       ImGuiWindowFlags_AlwaysAutoResize)) {
+                ImGui::Text("This will clear all existing mappings for\n%s (SDL %d) on port %d.\n\nContinue?",
+                            name.c_str(), sdlIndex, portIndex + 1);
+                if (ImGui::Button("Cancel")) {
+                    shouldClose = true;
+                    ImGui::CloseCurrentPopup();
+                }
+                if (ImGui::Button("Set defaults")) {
+                    Context::GetInstance()->GetControlDeck()->GetControllerByPort(portIndex)->ClearAllMappingsForDevice(
+                        lusIndex);
+                    Context::GetInstance()->GetControlDeck()->GetControllerByPort(portIndex)->AddDefaultMappings(
+                        lusIndex);
+                    shouldClose = true;
+                    ImGui::CloseCurrentPopup();
+                }
+                ImGui::EndPopup();
+            }
+        }
+
+        if (ImGui::Button("Cancel") || shouldClose) {
+            ImGui::CloseCurrentPopup();
+        }
+
+        ImGui::EndPopup();
+    }
+}
+
 void InputEditorWindow::DrawDevicesTab() {
     if (ImGui::BeginTabItem("Devices")) {
         std::map<LUSDeviceIndex, std::pair<std::string, int32_t>> indexMappings;
