d23be4c6a652da61a983dd7e774a58a5594510d5
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2023-12-23 17:21:06 -0500

    fix saving mappings for additional (port provided) bitmasks (#400)

--- src/controller/controldevice/controller/ControllerButton.h
+++ src/controller/controldevice/controller/ControllerButton.h
@@ -46,6 +46,7 @@ class ControllerButton {
     uint8_t mPortIndex;
     uint16_t mBitmask;
     std::unordered_map<std::string, std::shared_ptr<ControllerButtonMapping>> mButtonMappings;
+    std::string GetConfigNameFromBitmask(uint16_t bitmask);
 
     bool mUseKeydownEventToCreateNewMapping;
     KbScancode mKeyboardScancodeForNewMapping;
