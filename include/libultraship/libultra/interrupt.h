80813386b0fed83c34e20ed0b53b6ab594ccd4b0
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-01-05 20:07:15 -0500

    comment stuff out

--- include/libultraship/libultra/interrupt.h
+++ include/libultraship/libultra/interrupt.h
@@ -1,6 +1,6 @@
-#ifndef ULTRA64_INTERRUPT_H
-#define ULTRA64_INTERRUPT_H
+// #ifndef ULTRA64_INTERRUPT_H
+// #define ULTRA64_INTERRUPT_H
 
-typedef u32 OSIntMask;
+// typedef u32 OSIntMask;
 
-#endif
+// #endif
