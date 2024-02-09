b7387bbdc34be4dfeacaddae05966ce0398c4f4e
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-02-09 01:41:52 -0500

    matrix

--- src/resource/factory/MatrixFactory.cpp
+++ src/resource/factory/MatrixFactory.cpp
@@ -1,37 +1,31 @@
 #include "resource/factory/MatrixFactory.h"
 #include "resource/type/Matrix.h"
+#include "resource/readerbox/BinaryReaderBox.h"
 #include "spdlog/spdlog.h"
 
 namespace LUS {
-std::shared_ptr<IResource> MatrixFactory::ReadResource(std::shared_ptr<ResourceInitData> initData,
-                                                       std::shared_ptr<BinaryReader> reader) {
-    auto resource = std::make_shared<Matrix>(initData);
-    std::shared_ptr<ResourceVersionFactory> factory = nullptr;
-
-    switch (resource->GetInitData()->ResourceVersion) {
-        case 0:
-            factory = std::make_shared<MatrixFactoryV0>();
-            break;
-    }
-
-    if (factory == nullptr) {
-        SPDLOG_ERROR("Failed to load Matrix with version {}", resource->GetInitData()->ResourceVersion);
+std::shared_ptr<IResource> ResourceFactoryBinaryMatrixV0::ReadResource(std::shared_ptr<ResourceInitData> initData,
+                                                        std::shared_ptr<ReaderBox> readerBox) {
+    auto binaryReaderBox = std::dynamic_pointer_cast<BinaryReaderBox>(readerBox);
+    if (binaryReaderBox == nullptr) {
+        SPDLOG_ERROR("ReaderBox must be a BinaryReaderBox.");
         return nullptr;
     }
 
-    factory->ParseFileBinary(reader, resource);
+    auto reader = binaryReaderBox->GetReader();
+    if (reader == nullptr) {
+        SPDLOG_ERROR("null reader in box.");
+        return nullptr;
+    }
 
-    return resource;
-}
-
-void MatrixFactoryV0::ParseFileBinary(std::shared_ptr<BinaryReader> reader, std::shared_ptr<IResource> resource) {
-    std::shared_ptr<Matrix> mtx = static_pointer_cast<Matrix>(resource);
-    ResourceVersionFactory::ParseFileBinary(reader, resource);
+    auto matrix = std::make_shared<Matrix>(initData);
 
     for (size_t i = 0; i < 4; i++) {
         for (size_t j = 0; j < 4; j++) {
-            mtx->Matrx.m[i][j] = reader->ReadInt32();
+            matrix->Matrx.m[i][j] = reader->ReadInt32();
         }
     }
+
+    return matrix;
 }
 } // namespace LUS
