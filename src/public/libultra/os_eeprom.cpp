28a9b509adb7c0830dab913ce879847f38ade343
KiritoDv <kiritodev01@gmail.com>
2024-04-19 03:58:37 -0600

    Implemented eeprom support and fixed rumble
    (cherry picked from commit 0c3e1aca29d0c77ededad745ccbdb9ec21283828)

--- src/public/libultra/os_eeprom.cpp
+++ src/public/libultra/os_eeprom.cpp
@@ -23,6 +23,10 @@ int32_t osEepromLongRead(OSMesgQueue* mq, uint8_t address, uint8_t* buffer, int3
     return ret;
 }
 
+int32_t osEepromRead(OSMesgQueue* mq, u8 address, u8* buffer) {
+    return osEepromLongRead(mq, address, buffer, 8);
+}
+
 int32_t osEepromLongWrite(OSMesgQueue* mq, uint8_t address, uint8_t* buffer, int32_t length) {
     u8 content[512] = { 0 };
     if (address != 0 || length != 512) {
@@ -37,5 +41,9 @@ int32_t osEepromLongWrite(OSMesgQueue* mq, uint8_t address, uint8_t* buffer, int
     s32 ret = fwrite(content, 1, 512, fp) == 512 ? 0 : -1;
     fclose(fp);
     return ret;
+}
+
+int32_t osEepromWrite(OSMesgQueue* mq, uint8_t address, uint8_t* buffer) {
+    return osEepromLongWrite(mq, address, buffer, 8);
 }
 }
