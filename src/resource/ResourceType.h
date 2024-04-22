11af758d41dbcfdab25aef191d95d47585257a76
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-04-21 20:08:45 -0400

    move json resource/factory to LUS (#496)
    * move `RawJson` to LUS
    
    * rename

--- src/resource/ResourceType.h
+++ src/resource/ResourceType.h
@@ -14,5 +14,6 @@ enum class ResourceType {
     Array = 0x4F415252,       // OARR
     Blob = 0x4F424C42,        // OBLB
     Texture = 0x4F544558,     // OTEX
+    Json = 0x4A534F4E,        // JSON
 };
 } // namespace LUS
