a975ae22fe14434e89e025499ea5f1f09f09ea8c
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-04-30 17:09:56 -0400

    add a fetch content

---
+++ cmake/dependencies/mac.cmake
@@ -0,0 +1,13 @@
+#=================== spdlog ===================
+# macports has issues with this because of fmt
+# brew doesn't support building multiarch
+find_package(spdlog QUIET)
+if (NOT ${spdlog_FOUND})
+    FetchContent_Declare(
+        spdlog
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_TAG v1.14.1
+        OVERRIDE_FIND_PACKAGE
+    )
+    FetchContent_MakeAvailable(spdlog)
+endif()
