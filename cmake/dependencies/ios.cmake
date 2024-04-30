1f2dadf4cfbfa0f5e7759dc709e9db8275a25b6d
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-04-30 02:11:15 -0400

    spdlog from package managers

--- cmake/dependencies/ios.cmake
+++ cmake/dependencies/ios.cmake
@@ -36,6 +36,18 @@ if (NOT ${tinyxml2_FOUND})
     FetchContent_MakeAvailable(tinyxml2)
 endif()
 
+#=================== spdlog ===================
+find_package(spdlog QUIET)
+if (NOT ${spdlog_FOUND})
+    FetchContent_Declare(
+        spdlog
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_TAG v1.14.0
+        OVERRIDE_FIND_PACKAGE
+    )
+    FetchContent_MakeAvailable(spdlog)
+endif()
+
 #=================== libzip ===================
 find_package(libzip QUIET)
 if (NOT ${libzip_FOUND})
