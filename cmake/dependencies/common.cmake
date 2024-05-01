6ad0d2a439ed568337f9aad18bf88ff701f0ebec
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-04-30 21:41:20 -0400

    makeavailable is working now

--- cmake/dependencies/common.cmake
+++ cmake/dependencies/common.cmake
@@ -8,7 +8,7 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/ocornut/imgui.git
     GIT_TAG ce0d0ac8298ce164b5d862577e8b087d92f6e90e # docking 1.90.0
 )
-FetchContent_Populate(ImGui)
+FetchContent_MakeAvailable(ImGui)
 list(APPEND ADDITIONAL_LIB_INCLUDES ${imgui_SOURCE_DIR} ${imgui_SOURCE_DIR}/backends)
 
 add_library(ImGui STATIC)
