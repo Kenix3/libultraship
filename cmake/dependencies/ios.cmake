d7ae250c714af95e67a3dfd89b98566c7f942a10
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-05-02 11:55:50 -0400

    fetch metal-cpp (#549)

--- cmake/dependencies/ios.cmake
+++ cmake/dependencies/ios.cmake
@@ -69,13 +69,22 @@ if (NOT ${libzip_FOUND})
     list(APPEND ADDITIONAL_LIB_INCLUDES ${libzip_SOURCE_DIR}/lib ${libzip_BINARY_DIR})
 endif()
 
+#=================== Metal-cpp ===================
+FetchContent_Declare(
+    metalcpp
+    GIT_REPOSITORY https://github.com/briaguya-ai/single-header-metal-cpp.git
+    GIT_TAG macOS13_iOS16
+)
+FetchContent_MakeAvailable(metalcpp)
+list(APPEND ADDITIONAL_LIB_INCLUDES ${metalcpp_SOURCE_DIR})
+
 #=================== ImGui ===================
 target_sources(ImGui
     PRIVATE
     ${imgui_SOURCE_DIR}/backends/imgui_impl_metal.mm
 )
 
-target_include_directories(ImGui PRIVATE ${CMAKE_CURRENT_SOURCE_DIR}/extern/metal-cpp)
+target_include_directories(ImGui PRIVATE ${metalcpp_SOURCE_DIR})
 target_compile_definitions(ImGui PUBLIC IMGUI_IMPL_METAL_CPP)
 
 target_link_libraries(ImGui PUBLIC SDL2::SDL2-static SDL2::SDL2main)
