f55d3910e7ba29f6973512c15cde21d2a0b2e292
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2024-05-03 10:27:58 -0400

    fetch stb (#552)

--- cmake/dependencies/common.cmake
+++ cmake/dependencies/common.cmake
@@ -41,3 +41,18 @@ if(NOT EXCLUDE_MPQ_SUPPORT)
     FetchContent_MakeAvailable(StormLib)
     list(APPEND ADDITIONAL_LIB_INCLUDES ${stormlib_SOURCE_DIR}/src)
 endif()
+
+#=================== STB ===================
+set(STB_DIR ${CMAKE_BINARY_DIR}/_deps/stb)
+file(DOWNLOAD "https://github.com/nothings/stb/raw/0bc88af4de5fb022db643c2d8e549a0927749354/stb_image.h" "${STB_DIR}/stb_image.h")
+file(WRITE "${STB_DIR}/stb_impl.c" "#define STB_IMAGE_IMPLEMENTATION\n#include \"stb_image.h\"")
+
+add_library(stb STATIC)
+
+target_sources(stb PRIVATE
+    ${STB_DIR}/stb_image.h
+    ${STB_DIR}/stb_impl.c
+)
+
+target_include_directories(stb PUBLIC ${STB_DIR})
+list(APPEND ADDITIONAL_LIB_INCLUDES ${STB_DIR})
