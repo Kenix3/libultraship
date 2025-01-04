7d8f8f5d86759b9d030e189a0fba91265aa32e64
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2025-01-04 17:03:30 -0500

    start to try

--- cmake/dependencies/linux.cmake
+++ cmake/dependencies/linux.cmake
@@ -1,6 +1,6 @@
 #=================== ImGui ===================
-find_package(SDL2 REQUIRED)
-target_link_libraries(ImGui PUBLIC SDL2::SDL2)
+find_package(SDL3 REQUIRED)
+target_link_libraries(ImGui PUBLIC SDL3::SDL3)
 
 if (USE_OPENGLES)
     target_link_libraries(ImGui PUBLIC ${OPENGL_GLESv2_LIBRARY})
