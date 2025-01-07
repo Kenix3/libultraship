5224fe7f9adde5865022f6fe913ebda8c20ddaa9
coco875 <pereira.jannin@gmail.com>
2025-01-08 00:45:55 +0100

    progress on png

--- src/resource/factory/PngFactory.cpp
+++ src/resource/factory/PngFactory.cpp
@@ -54,6 +54,51 @@ std::shared_ptr<Ship::IResource> ResourceFactoryPngTexture::ReadResource(std::sh
     });
 
     png_read_info(png_ptr, info_ptr);
+    png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, nullptr);
+    texture->Width = png_get_image_width(png_ptr, info_ptr);
+    texture->Height = png_get_image_height(png_ptr, info_ptr);
+    texture->Type = TextureType::RGBA32bpp;
+    png_byte color_type = png_get_color_type(png_ptr, info_ptr);
+    png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+    // Read any color_type into 8bit depth, RGBA format.
+    // See http://www.libpng.org/pub/png/libpng-manual.txt
+
+    if (bit_depth == 16)
+        png_set_strip_16(png_ptr);
+
+    if (color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_palette_to_rgb(png_ptr);
+
+    // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
+    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+        png_set_expand_gray_1_2_4_to_8(png_ptr);
+
+    if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS))
+        png_set_tRNS_to_alpha(png_ptr);
+
+    // These color_type don't have an alpha channel then fill it with 0xff.
+    if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_PALETTE)
+        png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
+
+    if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
+        png_set_gray_to_rgb(png_ptr);
+
+    png_read_update_info(png_ptr, info_ptr);
+
+    texture->ImageDataSize = texture->Width * texture->Height;
+    texture->ImageData = new uint8_t[texture->ImageDataSize];
+
+    for (int y = 0; y < texture->Height; y++) {
+        png_bytep row = (png_bytep)texture->ImageData + (y * texture->Width);
+        png_read_row(png_ptr, row, nullptr);
+    }
+
+    png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+
+    texture->Flags = 0;
+
+    return texture;
 }
 
 } // namespace Fast
