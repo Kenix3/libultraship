0965986f17f0b92559e8dfec97bb010d3fcf6aab
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2025-01-10 03:59:00 -0500

    d3d11?

--- src/graphic/Fast3D/gfx_direct3d11.cpp
+++ src/graphic/Fast3D/gfx_direct3d11.cpp
@@ -1230,7 +1230,7 @@ gfx_d3d11_get_pixel_depth(int fb_id, const std::set<std::pair<float, float>>& co
 } // namespace
 
 ImTextureID gfx_d3d11_get_texture_by_id(unsigned long long id) {
-    return d3d.textures[id].resource_view.Get();
+    return (unsigned long long)d3d.textures[id].resource_view.Get();
 }
 
 void gfx_d3d11_enable_srgb_mode() {
