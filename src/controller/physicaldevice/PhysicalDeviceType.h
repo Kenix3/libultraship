4f95a27a219889a28143fb4f7b670caeb638eeee
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2025-01-19 16:49:03 -0500

    clang format

--- src/controller/physicaldevice/PhysicalDeviceType.h
+++ src/controller/physicaldevice/PhysicalDeviceType.h
@@ -2,11 +2,6 @@
 
 namespace Ship {
 
-enum PhysicalDeviceType { 
-    Keyboard = 0,
-    Mouse = 1,
-    SDLGamepad = 2,
-    Max = 3
-};
+enum PhysicalDeviceType { Keyboard = 0, Mouse = 1, SDLGamepad = 2, Max = 3 };
 
 } // namespace Ship
