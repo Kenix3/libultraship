40e7257e71a690693d24a58e41094d77829bd1b5
Spodi <spodi2290@aol.com>
2025-02-11 23:37:21 +0100

    V-sync Metal

--- src/graphic/Fast3D/gfx_sdl2.cpp
+++ src/graphic/Fast3D/gfx_sdl2.cpp
@@ -662,13 +662,13 @@ static inline void sync_framerate_with_timer() {
 }
 
 static void gfx_sdl_swap_buffers_begin() {
-    if (vsync_enabled !=
-        Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1)) {
+    if (vsync_enabled != Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1)) {
         // Make sure only 0 or 1 is set.
-        vsync_enabled =
-            !!Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1);
+        vsync_enabled = !!Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1);
     }
     SDL_GL_SetSwapInterval(vsync_enabled);
+    SDL_RenderSetVSync(renderer, vsync_enabled);
+
     sync_framerate_with_timer();
     SDL_GL_SwapWindow(wnd);
 }
