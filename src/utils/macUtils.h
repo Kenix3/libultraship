146d7cc7f3128c96d6a39fe7263bececa3e2a393
Lars-Christian Selland <larsy1995@gmail.com>
2025-02-13 16:41:54 +0100

    Feel free to shoot me. I'm a professional.

--- src/utils/macUtils.h
+++ src/utils/macUtils.h
@@ -4,8 +4,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-void toggleNativeMacOSFullscreen(SDL_Window *window);
-bool isNativeMacOSFullscreenActive(SDL_Window *window);
+void toggleNativeMacOSFullscreen(SDL_Window* window);
+bool isNativeMacOSFullscreenActive(SDL_Window* window);
 #ifdef __cplusplus
 }
 #endif
