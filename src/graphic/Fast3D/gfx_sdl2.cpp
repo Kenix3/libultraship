2b41d668caa98b2748934a7991c22707538b3a02
Spodi <spodi2290@aol.com>
2025-02-16 21:33:10 +0100

    Only call SetSwapInterval when cvar changes

--- src/graphic/Fast3D/gfx_sdl2.cpp
+++ src/graphic/Fast3D/gfx_sdl2.cpp
@@ -664,10 +664,11 @@ static inline void sync_framerate_with_timer() {
 static void gfx_sdl_swap_buffers_begin() {
     if (vsync_enabled != Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1)) {
         // Make sure only 0 or 1 is set.
-        vsync_enabled = !!Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1);
+        vsync_enabled =
+            (Ship::Context::GetInstance()->GetConsoleVariables()->GetInteger(CVAR_VSYNC_ENABLED, 1) ? 1 : 0);
+        SDL_GL_SetSwapInterval(vsync_enabled);
+        SDL_RenderSetVSync(renderer, vsync_enabled);
     }
-    SDL_GL_SetSwapInterval(vsync_enabled);
-    SDL_RenderSetVSync(renderer, vsync_enabled);
 
     sync_framerate_with_timer();
     SDL_GL_SwapWindow(wnd);
