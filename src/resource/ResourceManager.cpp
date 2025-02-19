28bb246294286fee92f375d821f678cb878a3af2
Archez <Archez@users.noreply.github.com>
2025-02-19 16:46:40 -0500

    Fix cache record for file load errors in resource manager (#823)

--- src/resource/ResourceManager.cpp
+++ src/resource/ResourceManager.cpp
@@ -143,6 +143,7 @@ std::shared_ptr<IResource> ResourceManager::LoadResourceProcess(const ResourceId
     auto file = LoadFileProcess(identifier.Path, initData);
     if (file == nullptr) {
         SPDLOG_TRACE("Failed to load resource file at path {}", identifier.Path);
+        mResourceCache[identifier] = ResourceLoadError::NotFound;
         return nullptr;
     }
 
