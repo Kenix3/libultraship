b5949b52ff71e25250b1b07f34bd9c760f2740a9
Tortuga veloz <danielcampora@outlook.es>
2025-03-18 12:59:46 +0100

    Fix format.

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -227,8 +227,7 @@ AudioBackend Config::GetCurrentAudioBackend() {
 }
 
 AudioChannelsSetting Config::GetCurrentAudioChannelsSetting() {
-    int32_t surround = GetInt("CVars." CVAR_AUDIO_CHANNELS_SETTING,
-        static_cast<int32_t>(AudioChannelsSetting::max));
+    int32_t surround = GetInt("CVars." CVAR_AUDIO_CHANNELS_SETTING, static_cast<int32_t>(AudioChannelsSetting::max));
     switch (surround) {
         case AudioChannelsSetting::surround51:
             return AudioChannelsSetting::surround51;
