4960284b580703078bb7b2e9b722fdac5b8738c3
louist103 <35883445+louist103@users.noreply.github.com>
2025-05-13 17:39:57 -0400

    Remove #if 0 block

--- src/graphic/Fast3D/backends/gfx_window_manager_api.h
+++ src/graphic/Fast3D/backends/gfx_window_manager_api.h
@@ -50,39 +50,5 @@ class GfxWindowBackend {
     bool mVsyncEnabled = true;
 };
 
-#if 0
 
-struct GfxWindowBackend {
-    void (*init)(const char* game_name, const char* gfx_api_name, bool start_in_fullscreen, uint32_t width,
-                 uint32_t height, int32_t posX, int32_t posY) = 0;
-    void (*close)() = 0;
-    void (*set_keyboard_callbacks)(bool (*mOnKeyDown)(int scancode), bool (*mOnKeyUp)(int scancode),
-                                   void (*mOnAllKeysUp)());
-    void (*set_mouse_callbacks)(bool (*mOnMouseButtonDown)(int btn), bool (*mOnMouseButtonUp)(int btn));
-    void (*set_fullscreen_changed_callback)(void (*on_fullscreen_changed)(bool is_now_fullscreen));
-    void (*set_fullscreen)(bool enable) = 0;
-    void (*get_active_window_refresh_rate)(uint32_t* refresh_rate) = 0;
-    void (*set_cursor_visibility)(bool visible) = 0;
-    void (*set_mouse_pos)(int32_t x, int32_t y) = 0;
-    void (*get_mouse_pos)(int32_t* x, int32_t* y) = 0;
-    void (*get_mouse_delta)(int32_t* x, int32_t* y) = 0;
-    void (*get_mouse_wheel)(float* x, float* y) = 0;
-    bool (*get_mouse_state)(uint32_t btn) = 0;
-    void (*set_mouse_capture)(bool capture) = 0;
-    bool (*mIsMouseCaptured)() = 0;
-    void (*get_dimensions)(uint32_t* width, uint32_t* height, int32_t* posX, int32_t* posY) = 0;
-    void (*handle_events)() = 0;
-    bool (*is_frame_ready)() = 0;
-    void (*swap_buffers_begin)() = 0;
-    void (*swap_buffers_end)() = 0;
-    double (*get_time)() = 0; // For debug
-    void (*set_target_fps)(int fps) = 0;
-    void (*set_maximum_frame_latency)(int latency) = 0;
-    const char* (*get_key_name)(int scancode) = 0;
-    bool (*can_disable_vsync)() = 0;
-    bool (*mIsRunning)() = 0;
-    void (*destroy)() = 0;
-    bool (*is_fullscreen)() = 0;
-};
-#endif
 #endif
