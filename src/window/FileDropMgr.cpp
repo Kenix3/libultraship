400ae86858ef80b4a9c04b0b26eaeb1c3a77b905
louist103 <35883445+louist103@users.noreply.github.com>
2025-06-23 12:18:07 -0400

    Add registration system

--- src/window/FileDropMgr.cpp
+++ src/window/FileDropMgr.cpp
@@ -5,6 +5,8 @@
 #ifdef _MSC_VER
 #define strdup _strdup
 #endif
+#include "Context.h"
+#include "Window.h"
 
 namespace Ship {
 FileDropMgr::~FileDropMgr() {
@@ -20,6 +22,7 @@ void FileDropMgr::SetDroppedFile(char* path) {
     }
     mPath = strdup(path);
     mFileDropped = true;
+    CallHandlers();
 }
 
 void FileDropMgr::ClearDroppedFile() {
@@ -37,5 +40,40 @@ bool FileDropMgr::FileDropped() const {
 char* FileDropMgr::GetDroppedFile() const {
     return mPath;
 }
+
+bool FileDropMgr::RegisterDropHandler(FileDroppedFunc func) {
+    for (const auto f : mRegisteredFuncs) {
+        if (func == f) {
+            //Dl_info info;
+            //dladdr(func, &info);
+            //SPDLOG_WARN("Trying to register {}. Already registered.", info.dli_sname);
+            return false;
+        }
+    }
+    mRegisteredFuncs.push_back(func);
+    return true;
+}
+
+bool FileDropMgr::UnregisterDropHandler(FileDroppedFunc func) {
+    for (auto it = mRegisteredFuncs.begin(); it != mRegisteredFuncs.end(); ++it) {
+        if (*it == func) {
+            mRegisteredFuncs.erase(it);
+            return true;
+        }
+    }
+    return false;
+}
+
+void FileDropMgr::CallHandlers() {
+    for (const auto f : mRegisteredFuncs) {
+        if (f(mPath)) {
+            return;
+        }
+    }
+    SPDLOG_WARN("Dropped file {} not handled by any registered.", mPath);
+    auto gui = Ship::Context::GetInstance()->GetWindow()->GetGui();
+    gui->GetGameOverlay()->TextDrawNotification(30.0f, true, "Unsupported file dropped, ignoring");
+}
+
 
 } // namespace Ship
