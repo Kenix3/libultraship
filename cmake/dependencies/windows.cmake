6d9984eecc54412947671a906ef4c7692c545d7a
Hal Kolb <hal@kolb.co.uk>
2025-08-09 11:39:40 +0100

    Fix incorrect windows dependency setup

--- cmake/dependencies/windows.cmake
+++ cmake/dependencies/windows.cmake
@@ -10,7 +10,3 @@ target_link_libraries(ImGui PUBLIC SDL2::SDL2 SDL2::SDL2main)
 
 find_package(GLEW REQUIRED)
 target_link_libraries(ImGui PUBLIC opengl32 GLEW::GLEW)
-
-find_path(LIBUSB_INCLUDE_DIR NAMES libusb.h PATH_SUFFIXES "libusb-1.0")
-target_include_directories(libultraship PUBLIC ${LIBUSB_INCLUDE_DIR})
-target_link_libraries(libultraship PUBLIC usb-1.0)
