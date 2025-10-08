37d63b1d75f1379a0ca1703d2d095d5c3a91e50f
briaguya <70942617+briaguya-ai@users.noreply.github.com>
2025-10-08 14:00:36 -0400

    more mac

--- src/fast/backends/gfx_metal_shader.cpp
+++ src/fast/backends/gfx_metal_shader.cpp
@@ -15,10 +15,10 @@
 #include <Metal/Metal.hpp>
 #undef TRUE
 #undef FALSE
-#include <public/bridge/consolevariablebridge.h>
+#include <ship/config/ConsoleVariable.h>
 #include "gfx_metal_shader.h"
 #include <prism/processor.h>
-#include "ship/resource/ResourceManager.h"
+#include <ship/resource/ResourceManager.h>
 #include "spdlog/spdlog.h"
 #include "../interpreter.h"
 // MARK: - String Helpers
