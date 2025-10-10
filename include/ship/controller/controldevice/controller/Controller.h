e658bc376263e00debee645fe25542d3f2258609
briaguya <70942617+briaguya0@users.noreply.github.com>
2025-10-10 04:55:02 -0400

    clean up includes (#928)

--- include/ship/controller/controldevice/controller/Controller.h
+++ include/ship/controller/controldevice/controller/Controller.h
@@ -7,8 +7,6 @@
 #include <queue>
 #include <vector>
 #include <map>
-#include "libultraship/libultra/controller.h"
-#include "libultraship/color.h"
 #include <unordered_map>
 #include "ControllerButton.h"
 #include "ControllerStick.h"
