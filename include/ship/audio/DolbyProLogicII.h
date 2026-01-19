5c2059b56117e836f15dd88785953c32c73a80c7
Tortuga Veloz <danielcampora@outlook.es>
2026-01-19 16:59:59 +0100

    Remove unneeded def of PI.

--- include/ship/audio/DolbyProLogicII.h
+++ include/ship/audio/DolbyProLogicII.h
@@ -4,10 +4,6 @@
 #include <cmath>
 #include <algorithm>
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
 namespace Ship {
 
 // =============================================================================
