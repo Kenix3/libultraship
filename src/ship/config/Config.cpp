27aec8e773983b59333a304606709ab0d2c3dd9a
Tortuga Veloz <danielcampora@outlook.es>
2026-01-23 09:51:54 +0100

    Add config.

--- src/ship/config/Config.cpp
+++ src/ship/config/Config.cpp
@@ -261,9 +261,9 @@ AudioBackend Config::GetCurrentAudioBackend() {
 }
 
 AudioChannelsSetting Config::GetCurrentAudioChannelsSetting() {
-    int32_t surround =
+    int32_t channels_setting =
         GetInt("CVars." CVAR_AUDIO_CHANNELS_SETTING, static_cast<int32_t>(AudioChannelsSetting::audioMax));
-    switch (surround) {
+    switch (channels_setting) {
         case AudioChannelsSetting::audioMatrix51:
             return AudioChannelsSetting::audioMatrix51;
         case AudioChannelsSetting::audioRaw51:
