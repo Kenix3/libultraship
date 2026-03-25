73e5f89614fcf0b93d4312c3b3c72d83b248a621
koplas <paul@schwabauer.co>
2026-03-25 14:31:14 +0100

    Revert "Improve ResourceManager cache lookup performance (#989)"
    This patch resulted in performance problems when using texture packs.
    
    See: #1025
    
    This reverts commit e05673606e57fc7079ed4dee59188f1cb1235efc.

--- include/ship/resource/ResourceManager.h
+++ include/ship/resource/ResourceManager.h
@@ -123,11 +123,12 @@ class ResourceManager {
   protected:
     std::shared_ptr<std::vector<std::shared_ptr<IResource>>> LoadResourcesProcess(const ResourceFilter& filter);
     void UnloadResourcesProcess(const ResourceFilter& filter);
-    const std::variant<ResourceLoadError, std::shared_ptr<IResource>>* CheckCache(const ResourceIdentifier& identifier);
-    const std::variant<ResourceLoadError, std::shared_ptr<IResource>>* CheckCache(const std::string& filePath);
+    std::variant<ResourceLoadError, std::shared_ptr<IResource>> CheckCache(const ResourceIdentifier& identifier,
+                                                                           bool loadExact = false);
+    std::variant<ResourceLoadError, std::shared_ptr<IResource>> CheckCache(const std::string& filePath,
+                                                                           bool loadExact = false);
 
-    std::shared_ptr<IResource>
-    GetCachedResource(const std::variant<ResourceLoadError, std::shared_ptr<IResource>>& cacheLine);
+    std::shared_ptr<IResource> GetCachedResource(std::variant<ResourceLoadError, std::shared_ptr<IResource>> cacheLine);
 
   private:
     std::unordered_map<ResourceIdentifier, std::variant<ResourceLoadError, std::shared_ptr<IResource>>,
