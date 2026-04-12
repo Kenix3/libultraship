5f2f19775c949af20137e47d71508d6583c10124
KiritoDv <kiritodev01@gmail.com>
2026-04-12 04:00:39 -0600

    Added better errors for compiler

--- src/ship/scripting/ScriptSystem.cpp
+++ src/ship/scripting/ScriptSystem.cpp
@@ -147,7 +147,13 @@ void ScriptSystem::Compile(const std::shared_ptr<Archive>& archive) {
         }
 
         tcc_set_error_func(s, NULL, [](void* opaque, const char* msg) {
-            throw std::runtime_error(std::string("Failed to compile script: ") + msg);
+            std::string_view sv(msg);
+            if (sv.find("error") != std::string_view::npos || sv.find("warning") != std::string_view::npos ||
+                sv.find("fatal") != std::string_view::npos) {
+                SPDLOG_ERROR("Compiler: {}", msg);
+            } else {
+                SPDLOG_INFO("Compiler: {}", msg);
+            }
         });
 
         tcc_define_symbol(s, "__DLL__", "1");
