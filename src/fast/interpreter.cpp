f23c7923cb0fe93016cc2d0ba5938e2645190972
KiritoDv <kiritodev01@gmail.com>
2026-06-24 00:50:17 -0600

    Fixed vtxs work always on GBI_FLOATS and regular

--- src/fast/interpreter.cpp
+++ src/fast/interpreter.cpp
@@ -4968,7 +4968,8 @@ bool gfx_vtx_hash_handler_custom(F3DGfx** cmd0) {
         F3DVtx* vtx = (F3DVtx*)sResourceManager->GetResourceRawPointer(hash);
 
         if (vtx != NULL) {
-            vtx = (F3DVtx*)((char*)vtx + offset);
+            constexpr size_t kN64VtxSize = 16;
+            vtx += offset / kN64VtxSize;
 
             (*cmd0)--;
             F3DGfx* cmd = *cmd0;
