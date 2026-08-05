1be1bdd88f9faccbb064a2440f535bf1b6174160
MegaMech <MegaMech@users.noreply.github.com>
2026-08-04 22:20:06 -0600

    Remove SetOriginalSize method
    Removed SetOriginalSize method from GuiWindow class.

--- include/ship/window/gui/GuiWindow.h
+++ include/ship/window/gui/GuiWindow.h
@@ -103,13 +103,6 @@ class GuiWindow : public GuiElement {
      */
     void Draw() override;
 
-   /**
-    * @brief Sets a windows initial size
-    */
-    void SetOriginalSize(ImVec2 size) {
-        mOriginalSize = size;
-    }
-
   protected:
     /**
      * @brief Overrides SetVisibility to also write the new state to the backing CVar.
